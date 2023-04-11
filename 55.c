859a37bbbe0a3b0cf9203297679089b5e0b9f5d2
정선우 <asergeant8860@gmail.com>
2023-04-11 06:54:04 +0000

    55/2.c

--- 55.c
+++ 55.c
@@ -4,8 +4,13 @@ int main() {
 	
 	int a = 3;
 	a = a + 6;
-	if(a > 5) printf("%d\n", a);
-	//printf("d\n", a - 6);
+	if(a < 5) { 
+		printf("%d\n", a);
+		printf("조건이 참일때, 실행됩니다.\n");
+	}else { 
+		printf("%d\n", a);
+		printf("조건이 참일때, 실행됩니다.\n");
+}
 	
 	return 0;
 }
