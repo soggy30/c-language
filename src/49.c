0933713335bdd80d85f1135d5821e38eddd38b04
정선우 <asergeant8860@gmail.com>
2023-04-11 07:07:59 +0000

    49

---
+++ src/49.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	
+	int a =30;
+	float b =3.14;
+	char c = 'A';
+	char d[6] = {'K','O','R','E','A', '\0'};
+	printf("%d %f %d %c", a, b, c, d[3]);
+	return 0;
+	
+}
