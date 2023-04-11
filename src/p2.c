94456e036211aee8f9658e3f88ca2c39b7414756
정선우 <asergeant8860@gmail.com>
2023-04-11 07:22:55 +0000

    p2

---
+++ src/p2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	
+	int num = 0;
+	printf("정수를 입력 : ");
+	scanf("%d", &num);
+	
+	if (num%2 == 0) {
+		printf("짝수입니다.\n");
+	}else {
+		printf("홀수입니다.\n");
+	}
+	
+	return 0;
+}
