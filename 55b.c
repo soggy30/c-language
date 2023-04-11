28d1dfbbfeec71561b2d8924ca11a9aa43936dcf
정선우 <asergeant8860@gmail.com>
2023-04-11 06:55:27 +0000

    55

---
+++ 55b.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	
+	int a = 3;
+	a = a + 6;
+	if(a > 5) { 
+		printf("%d\n", a);
+		printf("조건이 참일때, 실행됩니다.\n");
+	}else { 
+		printf("%d\n", a);
+		printf("조건이 참일때, 실행됩니다.\n");
+}
+	
+	return 0;
+}
